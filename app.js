c63f2d57f2215c13ec099b6c9b8c5584bb53c828
Dan Leonard <daniel@danleonard.us>
2020-03-08 00:27:08 -0600

    poop in my pants

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ var buildingList;
 console.log("Loading pg")
 const { Pool } = require('pg')
 const pool = new Pool()
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
 pool.query('SELECT * FROM buildings', (err, res) => {
   if (err) {
     throw err
   }
-  console.log('Building list:', res)
+  // console.log('Building list:', res)
   buildingList = res.rows
 })
 
@@ -25,6 +32,20 @@ app.set('view engine', 'ejs');
 
 // index page 
 app.get('/', function(req, res) {
+// async/await - check out a client
+  ;(async () => {
+    const client = await pool.connect()
+    try {
+      const res = await client.query('SELECT * FROM buildings')
+      buildingList = res.rows
+      console.log("Retrieved %d rows of buildings", buildingList.length)
+    } finally {
+      // Make sure to release the client before any error handling,
+      // just in case the error handling itself throws an error.
+      client.release()
+    }
+  })().catch(err => console.log(err.stack))
+
     res.render('pages/index',{
       buildings : buildingList
     });
